d40093416180bbe34bb1954e388a2bf0d5e0875d
David R Connell <davidconnell12@gmail.com>
2020-02-22 14:08:09 -0600

    Fix: Put labels into inline math

--- private/addAxisOptions.m
+++ private/addAxisOptions.m
@@ -5,11 +5,11 @@ function addAxisOptions(fid, h, width)
     fprintf(fid, '\t\t%s%s,\n', 'xmode=', h.XScale);
     fprintf(fid, '\t\t%s%0.8g,\n', 'xmin=', h.XLim(1));
     fprintf(fid, '\t\t%s%0.8g,\n', 'xmax=', h.XLim(2));
-    fprintf(fid, '\t\t%s%s%s,\n', 'xlabel={', h.XLabel.String, '}');
+    fprintf(fid, '\t\t%s%s%s,\n', 'xlabel={$', h.XLabel.String, '$}');
     fprintf(fid, '\t\t%s%s,\n', 'ymode=', h.YScale);
     fprintf(fid, '\t\t%s%0.8g,\n', 'ymin=', h.YLim(1));
     fprintf(fid, '\t\t%s%0.8g,\n', 'ymax=', h.YLim(2));
-    fprintf(fid, '\t\t%s%s%s\n', 'ylabel={', h.YLabel.String, '},');
+    fprintf(fid, '\t\t%s%s%s\n', 'ylabel={$', h.YLabel.String, '$},');
 
     if strcmp(h.Box, 'off')
         fprintf(fid, '\t\t%s\n', 'axis x line*=bottom,');
